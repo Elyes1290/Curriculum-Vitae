14e59f94c6b23b76d1fbd617e077a39ea06cb8cf
Elyes1290 <elyes.naitliman@gmail.com>
2022-03-11 16:56:22 +0100

    Version final de final officiel
    c'est une version OFFICIEL JAVASCRIPT

--- main.js
+++ main.js
@@ -1,31 +1,14 @@
-// $( ".change" ).on("click", function() {
-//     if( $( "body" ).hasClass( "dark" )) {
-//         $( "body" ).removeClass( "dark" );
-//         $( ".change" ).text( "OFF" );
-//     } else {
-//         $( "body" ).addClass( "dark" );
-//         $( ".change" ).text( "ON" );
-//     }
-// });
-
-// $( ".change" ).on("click", function() {
-//     if( $( "header" ).hasClass( "dark" )) {
-//         $( "header" ).removeClass( "dark" );
-//         $( ".change" ).text( "OFF" );
-//     } else {
-//         $( "header" ).addClass( "dark" );
-//         $( ".change" ).text( "ON" );
-//     }
-// });
-
-        // function myFunction() {
-        // var element = document.getElementById('header');
-        // element.classList.toggle("lightmode");
-        // }
-
-
-        let button = document.querySelector('.btn')
+        let button = document.querySelector('.ON')
+        button.value ='OFF'        
 
         button.addEventListener('click', ()=>{
+          if (button.value == "OFF"){
+            button.value = "ON"
+            button.innerHTML = "ON"
+          }
+          else{
+            button.value = "OFF"
+            button.innerHTML = "OFF"
+          }
           document.documentElement.classList.toggle('dark-mode')
         })
